3abdf923e246d2c86560da4913c94d95b6a0d484
Simon Blixenkrone <sblixenkrone@me.com>
2017-08-30 12:12:09 +0200

    bug bot test

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
+console.log("Start of bot script \n ::::::::::::::::");
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var slackBot = require('slack-bot')(https://hooks.slack.com/services/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);
 
 var app = express();
-var port = process.env.PORT || 4040;
+var port = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/',function(req, res){
+app.get('/', function(req, res) {
   res.status(200).send("This is a slack bug bot for Byrd");
 });
 
-console.log("error?");
-
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('listening on port' + port);
 });
 
-app.post('/hello', function(req, res, next){
+app.post('/hello', function(req, res, next) {
   var userName = req.body.user_name;
-  var botPayLoad = {
-    text: "hello " + userName
+  var bugRef = function() {
+    console.log("This is the bug # and link in Asana");
   };
+
+  var botPayLoad = {
+    text: "Hello " + userName + ", " + "your bug is submitted as # with link: *LINK*"
+  };
+
+
+  if (userName !== slackbot) {
+    return res.status(200).json(botPayLoad);
+  } else {
+    return res.status(200).end();
+  }
 });
 
-console.log("End of script");
+slackBot.send({
+  text: 'OMG look at this thing',
+  channel: '#bugbot',
+}, callback);
+
+
+console.log("End of bot script");
