2286c54d6bdd4ee599b95ff31914dc55cbbf1725
Simon Blixenkrone <sblixenkrone@me.com>
2017-08-30 10:26:40 +0200

    node setup

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
-asd();
+var app = express();
+var port = process.env.PORT || 4040;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/',function(req, res){
+  res.status(200).send("Hello World!");
+});
+
+app.listen(port, function(){
+  console.log('listening on port' + port);
+});
+
+app.post('/hello', function(req, res, next){
+  var userName = req.body.user_name;
+  var botPayLoad = {
+    text: "hello " + userName
+  };
+});
